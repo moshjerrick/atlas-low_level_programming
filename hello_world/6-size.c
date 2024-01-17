14942a802206e7e045b5701bc50829fbf808090b
root <moshjerrick@gmail.com>
2024-01-17 20:50:13 +0000

    6try1

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Print the size of various data types
+    printf("Size of char: %lu bytes\n", sizeof(char));
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+    printf("Size of short: %lu bytes\n", sizeof(short));
+    printf("Size of long: %lu bytes\n", sizeof(long));
+    printf("Size of long long: %lu bytes\n", sizeof(long long));
+
+    return 0;
+}
+
