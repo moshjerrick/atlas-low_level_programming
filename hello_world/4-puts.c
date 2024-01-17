fc27944e6100fc306a59801a0ac5d450cc59b4aa
root <moshjerrick@gmail.com>
2024-01-17 20:32:07 +0000

    4try6

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+include <stdio.h>
 /**
  * main - puts
  *
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
