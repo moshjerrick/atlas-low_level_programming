e076cca6f03a3d83367c9b7d0ef2210a7501efca
root <moshjerrick@gmail.com>
2024-01-17 20:23:15 +0000

    4try1

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#inclue <stdio.h>
+
+/**
+ * main-puts
+ *
+ * Return: Always0(Success)
+ */
+int main(void)
+{
+	puts("rogramming is like building a multilingual puzzle");
+	return (0);
+}
