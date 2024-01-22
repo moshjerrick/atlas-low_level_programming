52ba722b4ceaaeafeb8309d98463827f0048797f
root <moshjerrick@gmail.com>
2024-01-22 20:43:53 +0000

    try1

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabet
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int start = 'a';
+int end = 'z';
+
+int i;
+for (i = start; i <= end; ++i)
+putchar(i);
+
+start = 'A';
+end = 'Z';
+for (i = start; i <= end: ++i)
+putchar(i);
+
+
+putchar('\n');
+
+return (0);
+}
