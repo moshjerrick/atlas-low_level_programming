566802a28dd9a04ca7064e0c1191cf27e3703a7f
root <moshjerrick@gmail.com>
2024-01-22 20:35:05 +0000

    flp

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
 int start = 'a';
 int end = 'z';
 
 int i;
-for (i = start; i <= end; ++i) 
+for (i = start; i <= end; ++i)
 putchar(i);
 
 
