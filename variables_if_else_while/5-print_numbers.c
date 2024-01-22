08e79ca4dc01ecffee8e2b96183f9ae3190d184c
root <moshjerrick@gmail.com>
2024-01-22 21:03:11 +0000

    try1

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - printing single digit numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+printf("%d", i);
+
+return (0);
+}
