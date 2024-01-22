33126536683d43547de46e15bca7a636c1e8bdaa
root <moshjerrick@gmail.com>
2024-01-22 19:12:15 +0000

    try2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 int main() {
-// ASCII values for lowercase letters 'a' to 'z'
 int start = 'a';
 int end = 'z';
 
-// Iterate through the alphabet
+
 for (int i = start; i <= end; ++i) {
-// Print each letter using putchar
 putchar(i);
 }
 
-// Print a new line
+
 putchar('\n');
 
 return 0;
