16241a553ca7d711bfbc7f484ac04ceb07c22634
root <moshjerrick@gmail.com>
2024-01-22 21:52:19 +0000

    try

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - all numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar('i');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
