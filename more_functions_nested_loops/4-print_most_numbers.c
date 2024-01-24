551bca1f376da825565124ab184501bd2beadf70
root <moshjerrick@gmail.com>
2024-01-24 21:53:19 +0000

    72

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -3,17 +3,16 @@
 /**
  * main - 
  *
- * Return:
+ * Return: alwasys (0)
  */
 void print_most_numbers(void)
 {
 int i;
 
 for (i = 48; i < 58; i++)
-if (i !=50 && i != 52)
+if (i != 50 && i != 52)
 {
 putchar(i);
 }
 putchar('\n');
 }
-
