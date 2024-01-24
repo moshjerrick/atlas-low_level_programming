ffc3ebe3bb582bb936c0f3f1ec1fc0ef5a1d4c04
root <moshjerrick@gmail.com>
2024-01-24 20:57:25 +0000

    try2

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main
+ *
+ * Return: 
+ */
+void print_numbers(void)
+{
+int i
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+putchar('\n');
+}
+}
