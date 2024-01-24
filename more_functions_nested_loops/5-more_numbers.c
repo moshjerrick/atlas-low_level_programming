7a9368b1259f24ea1ba36633263938f074f73d95
root <moshjerrick@gmail.com>
2024-01-24 22:05:10 +0000

    try1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ * main - 
+ *
+ * Return:
+ */
+void more_numbers(void)
+{
+	int i
+	for (i = 0; i < 14; i++)
+	{
+		putchar(i * 10)
+	}
+	putchar('\n')
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,2 +1,3 @@
+void more_numbers(void)
 void print_most_numbers(void);
 int _putchar(char c);
