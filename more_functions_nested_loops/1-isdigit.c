77f0bc4fb3f85f7d1360ab8631e7ac0fb5717e7b
root <moshjerrick@gmail.com>
2024-01-24 19:19:30 +0000

    try2002

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
  * Return -
  */
-int _isdigit(int c)
-{
-return (c >= '0' && c <= '9');
+int _isdigit(int c) {
+    if (c >= '0' && c <= '9') {
+        return 1;
+    } else {
+        return 0;
+    }
 }
