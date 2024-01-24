3e71d0219bdc5b1c93307edd4e4458a1cb09163d
root <moshjerrick@gmail.com>
2024-01-24 21:36:21 +0000

    try1

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.>
+/**
+ * main - 
+ *
+ * Return:
+ */
+void print_most_numbers(void)
+{
+	int i;
+for (i = 48; i < 58: i++)
+{
+if (i !=50 && i != 52)
+{
+	putchar(i);
+}
+putchar('\n')
+}
+}
