f4e9b6afa09d973adfa41221fa15d7d4758ea5f9
root <moshjerrick@gmail.com>
2024-01-24 19:55:34 +0000

    thanks betty

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -4,10 +4,13 @@
  *
  * Return -
  */
-int _isdigit(int c) {
-    if (c >= '0' && c <= '9') {
-        return 1;
-    } else {
-        return 0;
-    }
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9') 
+{
+return 1;
+} else 
+{
+return 0;
+}
 }
