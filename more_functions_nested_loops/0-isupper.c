d5a2ff6be7ae8787e53ac135964396c2b5020d3b
root <moshjerrick@gmail.com>
2024-01-24 17:57:03 +0000

    try12

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <ctype.h>
 
+int _isupper(int c);
 
 /**
  * main - Entry point of the program.
  *
  * Return: Always 0 (success).
  */
-int _isupper(int c);
+int main(void)
 {
     char myChar = 'C';
 
     if (isupper(myChar))
     {
-        return 1;
+        return 1; // c is upper
     }
     else
     {
-        return 0; 
+        return 0; // c is lower
     }
 }
-
