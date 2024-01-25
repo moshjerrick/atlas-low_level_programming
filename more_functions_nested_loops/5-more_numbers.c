380cab7d37b8439a1a298fd15b3687e6ac5b7a4f
root <moshjerrick@gmail.com>
2024-01-25 19:20:01 +0000

    no

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,12 @@ for (i = 1; i <= 10; i++)
 {
 	for (j = 0; j <= 14; j++)
 {	
-if (j >= 10)
+		if (j >= 10)
 {
-	_putchar((j / 10) + 0);
+			_putchar((j / 10) + '0');
 			}
-			_putchar((j % 10) + 0);
+		_putchar((j % 10) + '0');
 }
 _putchar('\n');
-
 }
 }
