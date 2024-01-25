d50cee4ba8c011b8641af1f1553779b124011371
root <moshjerrick@gmail.com>
2024-01-25 20:27:57 +0000

    got it

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,20 +6,19 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int j;
-	{
-		for (i = 1; i <= n; i++)
-				{
-				for (j = 1; j < i; j++)
+int i;
+int j;
+{
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j < i; j++)
 				
-				_putchar(' ');
+_putchar(' ');
 				
-				_putchar('\\');
-				_putchar('\n');
-				}
-				}
-	if (n <= 0)
-		_putchar('\n');
-				}
-
+_putchar('\\');
+_putchar('\n');
+}
+}
+if (n <= 0)
+_putchar('\n');
+}
