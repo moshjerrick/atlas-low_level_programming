ae9b14776e412fc23cc6ea167abcd2fdab29c66f
root <moshjerrick@gmail.com>
2024-01-25 17:21:57 +0000

    try 2

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,11 @@
 void more_numbers(void)
 {
 
-for (i = 0; i < 14; i++)
-for (int j = 0; j <= 14; j++)
+for (int i = 0; i < 10; i++)
+{for (int j = 0; j <= 14; j++)
 {	putchar ('0' + j);
 
 }
-
+}
 putchar('\n');
 }
