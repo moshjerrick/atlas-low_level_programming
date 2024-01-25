b46026d0b90b4fbc138fd72c71b25ce0488e184d
root <moshjerrick@gmail.com>
2024-01-25 01:32:17 +0000

    ye3

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,20 +7,12 @@ void print_alpha();
 * main - program
 * Return: nothing
 */
-int main(void)
 {
-print_alpha();
-return (0);
-}
-
-/** print_alpha - prints alphabet
-* Return: null
-*/
-void print_alpha()
-{
+	void print_alphabet(void)
 int i;
 for(i = 97; i <= 122; i++)
-putchar(i);
-
+{
+	putchar(i);
+}
 putchar('\n'); 
 }
