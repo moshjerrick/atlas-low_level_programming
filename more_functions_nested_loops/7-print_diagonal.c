38a88155ab3f7a53f6a3d9befc63b3a120e82e85
root <moshjerrick@gmail.com>
2024-01-25 20:11:47 +0000

    try1

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main."
+/**
+ * main -
+ *
+ * Return:
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+	if (n <=0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++0
+				{
+				for (j = 0; j < i; j++)
+				{
+				_putchar(' ');
+				}
+				_putchar('\\');
+				_putchar('\n');
+				}
+				_putchar('\n')
+				}
+				}
