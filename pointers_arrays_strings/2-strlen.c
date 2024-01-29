2f3fd438eb80d54fe3caf9a1f4d4bb0482ce3359
root <moshjerrick@gmail.com>
2024-01-29 19:44:57 +0000

    try 5

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -9,13 +9,11 @@
  */
 int _strlen(char *s)
 {
-	 int length = 0;
-
-    while (*s != '\0')
-    {
-        length++;
-        s++;
-    }
-
-    return length;
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return length;
 }
