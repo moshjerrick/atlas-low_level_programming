72282509f3c7b945dedc4b682977d8fd5fb7b602
root <moshjerrick@gmail.com>
2024-01-29 18:13:44 +0000

    equals

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int i;
-for (i = 1; i <=100; i++)
+for (i = 1; i < 100; i++)
 {
 if (((i % 3) == 0) && ((i % 5) == 0))
 {
@@ -27,6 +27,5 @@ printf("%d ", i);
 }
 }
 printf("\n");
-puts("Buzz");
 return (0);
 }
