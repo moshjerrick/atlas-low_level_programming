fd244f9379adfcb3ca555a6a27c6b4bcfb44dde4
root <moshjerrick@gmail.com>
2024-01-29 21:23:34 +0000

    betty 2

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -4,18 +4,17 @@
 /**
  * puts_half - prints second half of line
  *
- * @str: pointer 
+ * @str: pointer
  *
  */
 void puts_half(char *str)
 {
-	int i;
-	int length = strlen(str);
-	int start_index = length / 2;
-
-		for (i = start_index; str[i] != '\0'; i++)
-		{
-			putchar(str[i]);
-		}
-		putchar('\n');
+int i;
+int length = strlen(str);
+int start_index = length / 2;
+for (i = start_index; str[i] != '\0'; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
 }
