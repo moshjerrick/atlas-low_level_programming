bb8b47d54e156edf04d0e1ab2b8442e9893f7394
root <moshjerrick@gmail.com>
2024-01-29 16:58:31 +0000

    try3

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,14 @@ int main(void)
 		{ 
 			printf("FizzBuzz");
 		}
-				else if ((i % 3) = 0)
+				else if ((i % 3) == 0)
 			{
 				printf("Fizz");
 			}
 			
-				else if ((i % 5) = 0)
+				else if ((i % 5) == 0)
 				{
-					printf("Buzz")
+					printf("Buzz");
 				}
 		return (0);
 		}
