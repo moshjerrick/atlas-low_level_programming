be94cee270e044d8c718bcc6fb620a6d1829d11d
root <moshjerrick@gmail.com>
2024-01-30 01:42:10 +0000

    try10

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,29 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
-int main (void)
-{
-    char s1[98];
-    char *ptr;
-    
-    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s", s1);
-    printf("%s", ptr);
-    return (0);
-}
-
 char *_strcpy(char *dest, char *src)
 {
-   _stpcopy(dest, src);
-   return dest;
-}
-           
-char *_stpcopy(char *dest, char *src)
-{
-   char  *p;
+    char *ptr = dest;
 
-   p = mempcpy(dest, src, strlen(src));
-   *p = '\0';
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
 
-   return p;
+    *dest = '\0';
+
+    return ptr;
 }
