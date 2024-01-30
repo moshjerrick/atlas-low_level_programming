2c900b238a188f5bc79fa330e65cd2da19f34a38
root <moshjerrick@gmail.com>
2024-01-30 01:47:36 +0000

    betty 4

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,9 +4,10 @@
 /**
  * *_strcpy - string copy
  *
- * @*dest: pointer to destination
- * @*src: pointer to source
- */
+ * @dest: pointer to destination
+ * @src: pointer to source
+ *
+ * Return: 0
 char *_strcpy(char *dest, char *src)
 {
 char *ptr = dest;
