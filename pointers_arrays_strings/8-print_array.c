ad56c0d3c87051d0a6fe52ab87ec0df2c8342495
root <moshjerrick@gmail.com>
2024-01-30 01:21:41 +0000

    try 1

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - 
+ *
+ *
+ */
+int main (void)
+void print_array(int *a, int n)
+{
+    int i;
+    for(i = 0; i < n - 1; i++)
+    {
+        printf("%d, ", a[i]); 
+    }
+    printf("%d", a[n - 1]);
+    
+    return;
+}
