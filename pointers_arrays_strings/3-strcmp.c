465659dcaff22ede4cc3b0bee1b851fa626cd724
root <moshjerrick@gmail.com>
2024-01-31 20:41:05 +0000

    try

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (sr1 != '\0' || sr2 != '\0')
+	{
+		if (sr1 != sr2) 
+		{
+			return (sr1 - sr2)
+		}
+	sr1++;
+	sr2++;
+	}
+	retun (0);
+}
