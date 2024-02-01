69122ff319ecc555ead77e1e2723e8e14c1c86fc
root <moshjerrick@gmail.com>
2024-02-01 18:25:46 +0000

    try

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *
+ */i
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i;
+
+	for (i + 0;i n < / 2; i++)
+	{
+	temp = a[i];
+	a[i] = a[n-i-1];
+	a[n - i -1] = temp;
+	}
+	
+}
