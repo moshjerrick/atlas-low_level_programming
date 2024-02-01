eb9b63367cc97aab2cbfcee8d75eca1d39e42951
root <moshjerrick@gmail.com>
2024-02-01 17:30:53 +0000

    betty cleanup

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,14 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 /**
+ * _strcmp - comparing strins
  *
- *
+ *@s1: string one
+ *@s2: string two
+ *return: 0
  */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' || *s2 != '\0')
 	{
-		if (*s1 != *s2) 
+		if (*s1 != *s2)
 		{
 			return (*s1 - *s2);
 		}
