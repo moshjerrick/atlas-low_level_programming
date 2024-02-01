76de8580dbd0009ef8850cdb0ed34cb8baefe89d
root <moshjerrick@gmail.com>
2024-02-01 19:19:13 +0000

    try 100

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -6,13 +6,13 @@
  *
  *
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int i;
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	if (str[i] >= 'a' && str[i] <= 'z')
-		str[i] = (int)stri[i] - 32;
+		str[i] = (int)str[i] - 32;
 	}
 	return (str);
 }
