b8dba917995c08816b312a841fcccc64d6fd1704
root <moshjerrick@gmail.com>
2024-02-05 21:43:48 +0000

    try2

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *ptr_accept = accept;
+		while (*prt_accept != '\0')
+		{
+			if (*s == *ptr_accept)
+			{
+				return s;
+			}
+			ptr_accept++;
+		}
+		s++;
+	}
+	return NULL;
+}
