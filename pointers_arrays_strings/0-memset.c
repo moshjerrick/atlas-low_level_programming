946ec31ede0657d557d9ca0a3baea8e259544287
root <moshjerrick@gmail.com>
2024-02-05 18:55:46 +0000

    betty2

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+/**
+ *function _memset - setting first num bytes
+ *@s: pointer
+ *@b: char 
+ *@n: int
+ *
+ *Return: (s)
+ */
+
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
