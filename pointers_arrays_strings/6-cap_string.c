1a0caa60bb789f0d601906517692e27749afc70a
root <moshjerrick@gmail.com>
2024-02-05 18:12:17 +0000

    try 1

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.>
+#include <sting.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	if (str[i])
+	{
+		str[i] = toupper((unsigned char)str[i];
+	}
+	while (str[i])
+	}
+	if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+            str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}') 
+	{
+            i++;
+	    if (str[i])
+	    {
+	    str[i] = toupper((unsigned char)str[i]);
+	    }
+	    }
+	    else
+	    {
+	    i++;
+	    }
+	    }
+	    return str;
+}
