7ab98a159e9f82b592f6e1115cd44999a9c1cc28
root <moshjerrick@gmail.com>
2024-02-05 21:32:52 +0000

    try2

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -13,13 +13,13 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	int found;
 
-	for (i = =; s[i] != 0; i==)
+	for (i = 0; s[i] != 0; i++)
 	{
 		found = 0;
 
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accetpt[j])
+			if (s[i] == accept[j])
 			{
 		found = 1;
 		break;
