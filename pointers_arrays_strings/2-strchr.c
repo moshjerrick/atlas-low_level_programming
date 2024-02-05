bea857240bace87a00bff99014c46a2c02228ee9
root <moshjerrick@gmail.com>
2024-02-05 19:23:33 +0000

    try1

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+	if (*s == c)
+	{
+		return s;
+	}
+	s++;
+	}
+	reutn NULL;
+}
