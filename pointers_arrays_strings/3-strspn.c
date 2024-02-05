b40b2214fff2cd0820e79a40084ddc37dce2134b
root <moshjerrick@gmail.com>
2024-02-05 22:22:47 +0000

    betty 69

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,27 +9,27 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count = 0;
-	int i, j;
-	int found;
+unsigned int count = 0;
+int i, j;
+int found;
 
-	for (i = 0; s[i] != 0; i++)
-	{
-		found = 0;
+for (i = 0; s[i] != 0; i++)
+{
+found = 0;
 
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-		found = 1;
-		break;
-			}
-		}
-		if (found == 0)
-		{
-			return (count);
-		}
-		count++;
-	}
-	return (count);
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+found = 1;
+break;
+}
+}
+if (found == 0)
+{
+return (count);
+}
+count++;
+}
+return (count);
 }
