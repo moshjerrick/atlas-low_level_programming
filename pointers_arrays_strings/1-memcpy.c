edc9d13e71f0596e54f03861a27a41f56f741665
root <moshjerrick@gmail.com>
2024-02-05 19:10:10 +0000

    tyr1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++);
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
