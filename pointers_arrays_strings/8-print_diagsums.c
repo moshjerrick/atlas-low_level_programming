ec8b8d788a17647de97634159e9000a617fdbf0c
root <moshjerrick@gmail.com>
2024-02-05 22:19:40 +0000

    try 2

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0, sum2 = 0;
+
+	for (i =0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size -1 -i));
+	}
+	printf("sum of fist diagnal: %d/n", sum1);
+	printf("sum of second diagonal: %d/n" sum2);
+}
