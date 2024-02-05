4fa2de8bff11bc0edd90eb02e6412552730d7e36
root <moshjerrick@gmail.com>
2024-02-05 21:31:41 +0000

    try1

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *
+ *
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i, j;
+	int found;
+
+	for (i = =; s[i] != 0; i==)
+	{
+		found = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accetpt[j])
+			{
+		found = 1;
+		break;
+			}
+		}
+		if (found == 0)
+		{
+			return count;
+		}
+		count++;
+	}
+	return (count);
+}
