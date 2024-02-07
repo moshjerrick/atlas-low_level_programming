4cf8745a1a77da6d4ce68625fc77f8d8311d5051
root <moshjerrick@gmail.com>
2024-02-07 19:31:31 +0000

    betty

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
+ *factorial - retrieving factorial of a given number
  *
- *
- *
+ *@n: interger
  */
 int factorial(int n)
 {
 	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
 	else if (n == 0)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return n * factorial(n - 1);
+		return (n * factorial(n - 1));
 }
 }
