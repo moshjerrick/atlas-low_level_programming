2f90abebc245a729bd775bd51bcc772a7189ca0b
root <moshjerrick@gmail.com>
2024-02-07 22:49:36 +0000

    betty again

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,12 +9,12 @@
  */
 int is_prime_number(int n)
 {
-	if (n <= 1) 
+	if (n <= 1)
 		return (0);
-	if (n <= 3) 
-		return (1);
+	if (n <= 3)
+		return (1)
 
-	if (n % 2 == 0 || n % 3 == 0) 
+	if (n % 2 == 0 || n % 3 == 0)
 		return (0);
 
 	return (1);
