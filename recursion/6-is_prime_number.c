58a0672820344181f2ea011be2210a16f5991719
root <moshjerrick@gmail.com>
2024-02-07 22:45:06 +0000

    try 4

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,18 +9,10 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-	
-	if (n < 0)
-	{
-		return (0);
-	}
-	for (i = 2; i <= sqrt(n); i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
+	if (n <= 1) return (0);
+	if (m <= 3) return (1);
+
+	if (n % 2 == 0 || n % 3 == 0) return (0);
+
 	return (1);
-}	
+}
