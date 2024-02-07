0fa28d20ea294a0079cabaa1cece43378911f51a
root <moshjerrick@gmail.com>
2024-02-07 18:07:40 +0000

    try 1

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar('\n');
+	return;
+	}
+	putchar(*s);
+	_puts_recursion(str);
+	return (0);
+}
