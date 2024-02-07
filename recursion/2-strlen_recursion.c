7659a5558975795bcd4e8bdee1b108e6a9ae31e0
root <moshjerrick@gmail.com>
2024-02-07 21:44:31 +0000

    betty

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
+ *_strlen_recursion - returning length of a string
  *
- *
- *
- *
+ *@s: pointer
+ *Return: 0
  */
 int _strlen_recursion(char *s)
 {
@@ -11,5 +11,5 @@ int _strlen_recursion(char *s)
 	{
 		return (0);
 	}
-	return 1 +  _strlen_recursion(s + 1);
+	return (1 +  _strlen_recursion(s + 1));
 }
