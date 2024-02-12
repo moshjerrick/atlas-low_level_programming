459a2df5b4ad54d6962b9a4c6c902b6cba2ff8e9
root <moshjerrick@gmail.com>
2024-02-12 22:19:43 +0000

    100

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++) {
+int main(int argc, char *argv[]) 
+{
+    int i;
+
+	for (i = 0; i < argc; i++) 
+	{
         printf("%s\n", argv[i]);
     }
     return 0;
