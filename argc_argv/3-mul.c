f37119cb0aa041122bcd80694902ca44c873a1a6
root <moshjerrick@gmail.com>
2024-02-12 22:45:17 +0000

    try3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,12 +14,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	int num1 = _atoi(argv[1]);
+	int num2 = _atoi(argv[2]);
 
 	int result = num1 * num2;
 
 	printf("%d\n", result);
 
-	return (0):
+	return (0);
 }
