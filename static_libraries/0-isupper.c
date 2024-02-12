01cd74d681c3bd53a7c5eeb97d74e9178e6709e3
root <moshjerrick@gmail.com>
2024-02-12 19:06:09 +0000

    1000

--- static_libraries/0-isupper.c
+++ static_libraries/0-isupper.c
@@ -1,1 +1,19 @@
-int _isupper(int c);
+#include "main.h"
+/**
+ * _isupper - Entry point of the program.
+ *
+ * @c: is upper case?
+ * Return: Always 0 (success).
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
