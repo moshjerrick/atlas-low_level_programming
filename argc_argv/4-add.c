cfdeea3d89d47630c69e29c387d6894497ae2540
root <moshjerrick@gmail.com>
2024-02-12 22:53:26 +0000

    try1

---
+++ argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc, char *argv[]) {
+    int i;
+    int sum;
+
+    if (argc == 1) {
+        printf("0\n");
+        return 0;
+    }
+
+    
+    int sum = 0;
+
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                printf("Error\n");
+                return 1;
+            }
+        }
+
+        
+        sum += atoi(argv[i]);
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
+
