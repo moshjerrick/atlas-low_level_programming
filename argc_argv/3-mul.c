55881e1b41dc34a187a349efa2012a39bc2600ae
root <moshjerrick@gmail.com>
2024-02-12 22:43:14 +0000

    tyr1

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int main(argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0):
+}
