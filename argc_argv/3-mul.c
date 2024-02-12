20aedd4e7df4cdb32bc4f5c9d28e3504bc98ce83
root <moshjerrick@gmail.com>
2024-02-12 22:47:52 +0000

    try 4

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,13 +9,16 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1;
+	int num2;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int num1 = _atoi(argv[1]);
-	int num2 = _atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 
 	int result = num1 * num2;
 
