53b763651f067e9e5afce7aa650ec76c9a342e6f
root <moshjerrick@gmail.com>
2024-02-13 01:40:59 +0000

    betty

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -10,9 +10,9 @@
  */
 int main(int argc, char *argv[])
 {
-    	if (strlen(argv[0]) > 0)
-			{
-	printf("%d\n", argc - 1);
+	if (strlen(argv[0]) > 0)
+	{
+		printf("%d\n", argc - 1);
 	}
 	return (0);
 }
