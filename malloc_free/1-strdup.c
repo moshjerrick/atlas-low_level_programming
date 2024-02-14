1cb19a0ef24ba0cba529676a6cf2ceee34658811
root <moshjerrick@gmail.com>
2024-02-14 19:47:04 +0000

    try2

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,13 +12,14 @@ char *_strdup(char *str)
 {
 	int length;
 	int i;
+	char *duplicate;
 
 	if (str == NULL)
 		return (NULL);
 	length = 0;
 	while (str[length] != '\0')
 		length ++;
-	char *duplicate = (char *)malloc((length = 1) * sizeof(char);
+	*duplicate = (char *)malloc((length = 1) * sizeof(char);
 			if (duplicate == NULL)
 			return (NULL);
 			for (i = 0; i < length; i ++)
