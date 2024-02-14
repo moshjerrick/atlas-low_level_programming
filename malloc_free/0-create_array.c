a076809fd9e6179e020c90e7a3489f5f95a6b6ca
root <moshjerrick@gmail.com>
2024-02-14 18:49:12 +0000

    try3

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,12 +10,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+
 	if (size == 0)
 		return (NULL);
 	char *array = (char *)malloc(size * sizeof(char));
 	if (array = NULL)
 		return (NULL);
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		array[i] = c;
 	return (array);
 }
