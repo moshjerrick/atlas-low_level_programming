13eb250879ef3c5fadfde60f81a90c153cf32886
root <moshjerrick@gmail.com>
2024-02-14 19:18:42 +0000

    betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ *create_array - creates an array of chars
  *
- *
- *
- *
- *
+ *@size: unsigned int
+ *@c: int
+ *Return: pointer of NULL
  */
 char *create_array(unsigned int size, char c)
 {
