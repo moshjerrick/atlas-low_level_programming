7053c956881ebc8546e317110f84c37d3d7ba18b
root <moshjerrick@gmail.com>
2024-02-19 17:24:10 +0000

    shep

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmeb == 0) || size == 0)
+		return (NULL);
+	if (size >= UINT_MAX / nmemb || nmemb >= UINT_MAX / size)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
+	return ((void *)p);
+}
