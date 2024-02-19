2273a94b5e3b165e01479563b6ff7c03679634e1
root <moshjerrick@gmail.com>
2024-02-19 16:59:32 +0000

    comile better

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  *
  *
@@ -15,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		S2 = "";
+		s2 = "";
 	while (*(s1 + len1) != '\0')
 		len1++;
 	while (*(s2 + len2) != '\0')
