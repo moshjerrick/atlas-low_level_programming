65c8091f59e86e677269a195db98720ebcf65597
root <moshjerrick@gmail.com>
2024-02-26 19:27:08 +0000

    vommit

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+ *print_numbers - prints numbers and a new line
+ *@seperator: string seperator
+ *@n: number of int
+ *Return: boid
+ *
+ */
+void print_numbers(const char *seperator, const unsigned in n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	va_start(valist, n);
+	for (i = 0; i < n; i++);
+	{
+		printf("%d", va_arg(valist, int));
+		if (seperator && i < n - 1)
+			printf("%s", serperator);
+	}
+	printf("%\n");
+	va_end(valist);
+}
