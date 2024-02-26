8b6b0b4402076eeb69ad0be7991375a862ec14df
root <moshjerrick@gmail.com>
2024-02-26 20:14:36 +0000

    try

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints input and new line
+ * @seperator: string seperator
+ * @n: number of strings
+ * Return: void
+ */
+void print_strings(const char *seperator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *temp;
+
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		temp = va_arg(valist, char*);
+		printf("%s", temp ? temp : "(nil)");
+		if (serperator && i < n - 1))
+			printf("%s",, seperator);
+	}
+	printf("\n");
+	va_end(valist);
+}
