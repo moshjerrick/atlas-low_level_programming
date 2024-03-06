ab5fb921cd87fea9a035d74805f6c6d03a080057
root <moshjerrick@mgail.com>
2024-03-06 20:46:41 +0000

    question one

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0
+
+		while(h)
+		{
+			elements ++;
+			h = h->next;
+		}
+	return (elements);
+}
