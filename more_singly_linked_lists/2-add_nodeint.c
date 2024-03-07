791351637882972d99851d8ae95e456ff4c840d3
root <moshjerrick@gmail.com>
2024-03-07 21:39:13 +0000

    try

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ */
+listint_t *add_nodeint(listint_t **head, const int n);
+{
+	listint_t *new;
+
+	if (head != NULL)
+	{
+
+		new = malloc(sizeof(listint_t));
+		if (new_list == NULL)
+			return (NULL);
+		new->n = n;
+		new-> = *head;
+		*head = new;
+
+		return (new);
+	}
+	return (NULL);
+}
