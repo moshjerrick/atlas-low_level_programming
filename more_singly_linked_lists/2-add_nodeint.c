657eb811dcfd304c9e696d0d65dca030e62533a8
root <moshjerrick@gmail.com>
2024-03-07 21:42:19 +0000

    uhhh again

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -14,10 +14,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	{
 
 		new = malloc(sizeof(listint_t));
-		if (new_list == NULL)
+		if (new == NULL)
 			return (NULL);
 		new->n = n;
-		new-> = *head;
+		new->next = *head;
 		*head = new;
 
 		return (new);
