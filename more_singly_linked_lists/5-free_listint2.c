a39e9236cdff7ff635584f84c6069c9ac57d7656
root <moshjerrick@gmail.com>
2024-03-11 19:53:51 +0000

    here take this

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	head = NULL;
+}
