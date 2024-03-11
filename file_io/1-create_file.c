fe491a6830923d1ff728bb478c41230bd37530f5
root <moshjerrick@gmail.com>
2024-03-11 21:06:00 +0000

    different approach

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,26 +5,32 @@
  *
  *
  */
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
 
 	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		return (0);
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
-		return (-1);
-
-	close(o);
-
-	return (1);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	free(buff);
+	return (wd);
 }
