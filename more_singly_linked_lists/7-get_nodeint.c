eab59a91808d5b52646c613f60adc11dbb5fe058
root <moshjerrick@gmail.com>
2024-03-11 22:40:56 +0000

    betty betty betty

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ *get_nodeint_at_index - locates node in linked list
+ *@head: pointer to head of node
+ *@index: the index of the node to locate
+ *Return: if no node = null, or the located node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
