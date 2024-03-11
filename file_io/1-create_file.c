ef8b5925559d2b003776b8ba757c368ca80c65b6
root <moshjerrick@gmail.com>
2024-03-11 21:57:52 +0000

    try 2

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
  *
  *
@@ -10,8 +12,15 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, rd, wd;
 	char *buff = malloc(sizeof(char) * letters);
+	size_t letters = 1024;
 
 	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
 	{
 		return (0);
 	}
@@ -19,16 +28,22 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR);
 	if (fd == -1)
 	{
+		free(buff);
 		return (0);
 	}
+	
 	rd = read(fd, buff, letters);
 	if (rd == -1)
 	{
+		close(fd)
+		free(buff);	
 		return (0);
 	}
 	wd = write(STDOUT_FILENO, buff, rd);
 	if (wd == -1)
 	{
+		close(fd);
+		free(buff);
 		return (0);
 	}
 	close(fd);
