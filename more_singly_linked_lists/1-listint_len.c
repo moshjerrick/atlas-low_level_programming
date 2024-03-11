c23a29157ba6b127570fd6e6a5eaade1be0aa658
root <moshjerrick@gmail.com>
2024-03-11 20:10:18 +0000

    betty

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
 /**
+ *listint_len - length of listint
+ *@h: pointer 
  *
- *
- *
- *
+ *Return: lenth in nodes
  */
 size_t listint_len(const listint_t *h)
 {
