683260747848db35e6988817c934d4cb5e15193b
JOSH MERRICK <moshjerrick@gmail.com>
2024-03-14 14:02:58 -0500

    missed an n

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
     {
         return (NULL);
     }
-    new_node-> = n;
+    new_node->n = n;
     new_node->prev = NULL;
 
     if (*head != NULL)
