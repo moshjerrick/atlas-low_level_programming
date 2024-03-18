f2f3801b20fd7aa32aee7cf9228790637bb5ff8e
JOSH MERRICK <moshjerrick@gmail.com>
2024-03-18 12:48:23 -0500

    has != hash

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,7 +9,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
     unsigned long int hash_value;
 
-    hash_value = has_djb2(key);
+    hash_value = hash_djb2(key);
 
     return (hash_value % size);
 
