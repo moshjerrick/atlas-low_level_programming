d9489c1b97dff4c2f121001012f09da5bd7569ca
JOSH MERRICK <moshjerrick@gmail.com>
2024-03-18 16:47:25 -0500

    removed unneeded liine

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -23,7 +23,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			free(current);
 			return (1);
 		}
-		temp = *head;
+
 
 		for (i = 0; current != NULL && i < index; i++)
 		{
