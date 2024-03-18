0be8eea237b26ffc45ec57aca5d4701fab77252e
JOSH MERRICK <moshjerrick@gmail.com>
2024-03-18 16:21:41 -0500

    more betty

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
+ * hash_table_print - prints hash table
+ * @ht: hash table to print
  * 
- * 
- * 
- * 
+ * Return: nada
 */
 void hash_table_print(const hash_table_t *ht)
 {
