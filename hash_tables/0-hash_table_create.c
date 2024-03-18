5e2000c6ab30ebe1124131370f29e88afa8bf89f
JOSH MERRICK <moshjerrick@gmail.com>
2024-03-18 12:17:54 -0500

    betty fixes

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,34 +1,35 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 /**
- * 
- * 
- * 
- * 
+ * hash_table_t - creates hash table
+ * @size: size of array
+ *
+ * Return: pointer to new hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table = NULL;
-    unsigned long int i;
+	hash_table_t *hash_table = NULL;
+	unsigned long int i;
 
-    hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-    {
-        return (NULL);
-    }
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
 
-    hash_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (hash_table->array == NULL)
-    {
-        free(hash_table);
-        return (NULL);
-    }
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        hash_table->array[i] = NULL;
-    }
-    hash_table->size = size;
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+	hash_table->size = size;
 
-    return (hash_table);
+	return (hash_table);
+
 }
