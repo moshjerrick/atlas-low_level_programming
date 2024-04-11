1ef3ae97ca63fe7853897e85c208a1c9854956a9
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-11 12:05:29 -0500

    fixed for loop

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,12 +1,11 @@
 #include <stddef.h>
 
-int i = 0;
-
 int linear_search(int *array, size_t size, int value) {
     if (array == NULL || size == 0) {
         return -1; 
 
-    for (size_t i; i < size; ++i) {
+    size_t i;
+    for (i = 0; i < size; ++i) {
         if (array[i] == value) {
             return i; 
         }
