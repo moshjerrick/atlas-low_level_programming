501cd8a670ff872a5be8db111ecaf50fa60c3dcf
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-11 12:03:50 -0500

    changed for loop i declaration

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,7 +6,7 @@ int linear_search(int *array, size_t size, int value) {
     if (array == NULL || size == 0) {
         return -1; 
 
-    for (size_t i = 0; i < size; ++i) {
+    for (size_t i; i < size; ++i) {
         if (array[i] == value) {
             return i; 
         }
