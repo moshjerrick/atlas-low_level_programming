e910f54f91702122bb80748765ce230d4c8d454b
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-11 12:07:15 -0500

    uhh

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,10 +1,12 @@
 #include <stddef.h>
 
+size_t i;
+
 int linear_search(int *array, size_t size, int value) {
     if (array == NULL || size == 0) {
         return -1; 
 
-    size_t i;
+
     for (i = 0; i < size; ++i) {
         if (array[i] == value) {
             return i; 
