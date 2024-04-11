025d73c818745744586696a8d39a7c9aa3f9a664
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-11 12:11:40 -0500

    added more parenthises around returns and missing squigly

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,14 +4,15 @@ size_t i;
 
 int linear_search(int *array, size_t size, int value) {
     if (array == NULL || size == 0) {
-        return -1; 
+        return (-1);
 
 
     for (i = 0; i < size; ++i) {
         if (array[i] == value) {
-            return i; 
+            return (i);
         }
     }
 
     return (-1);
+ }
 }
