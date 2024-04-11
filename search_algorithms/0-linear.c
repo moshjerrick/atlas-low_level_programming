b71f165a0d196c04aeb68cd50418c33be6f8b733
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-11 12:30:58 -0500

    new approach

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,19 +1,22 @@
 #include <stddef.h>
+#include "search_algos"
 
-size_t i;
 
-int linear_search(int *array, size_t size, int value) {
+int linear_search(int *array, size_t size, int value)
+{
+    int i = 0;
 
-	if (array == NULL || size == 0) {
-		return (-1);
-    }
+	if (array == NULL)
+        return (-1);
 
-	for (i = 0; i < size; ++i) {
-		if (array[i] == value) {
-			return (i);
-		}
-	}
-
-	return (-1);
-}
-
+        while (i < (int)size)
+        {
+            if (array[i] == value)
+            {
+            return (i);
+        }
+        else
+        i++:
+        }
+        return (-1);
+        }
