61d0a6b376cbba2580d59c51186b166c749065ed
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-18 12:41:07 -0500

    added 6 hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+* hash_table_delete - Deletes a hash table.
+* @ht: Pointer to the hash table.
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	hash_node_t *temp, *next;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			next = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
