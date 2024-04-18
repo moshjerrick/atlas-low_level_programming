a4408ac831d3b51c042305db77e83d3202b1e617
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-18 12:35:55 -0500

    notation added

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 /**
- * hash_table_t - creates hash table
- * @size: size of array
+ * hash_table_create - Function that creates a hash table.
+ * @size: size of the array.
  *
- * Return: pointer to new hash table
-*/
+ * Return: pointer to the hash table.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
