7fabb04cc248725034db47b7f87c9eb1f29df6a0
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-18 14:49:32 -0500

    i hate spaces in places they aint supposed to be

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,36 +1,41 @@
 #include "lists.h"
+
 /**
- *
- * 
- * 
+* add_dnodeint_end - Adds a new node at the end of a doubly linked list.
+*
+* @head: Pointer to a pointer to the head of the list.
+* @n: Value to be assigned to the new node.
+*
+* Return: Address of the new node, or NULL if it fails.
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *last_node;
+	dlistint_t *new_node, *last_node;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-        return (new_node);
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 
-    }
-    last_node = *head;
-    while (last_node->next != NULL)
-    {
-        last_node = last_node->next;
-    }
-    last_node->next = new_node;
-    new_node->prev = last_node;
-    
-    return (new_node);
+	last_node = *head;
+	while (last_node->next != NULL)
+	{
+		last_node = last_node->next;
+	}
+
+	last_node->next = new_node;
+	new_node->prev = last_node;
+
+	return (new_node);
 }
