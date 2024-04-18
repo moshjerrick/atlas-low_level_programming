33fa4dcfc5d82cc524cd470d95a596ff25ef3d69
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-18 14:41:25 -0500

    beetty fixes

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 /**
- * 
- * 
- * 
- * 
-*/
+ * add_dnodeint - Adds a new node at the beginning of a doubly linked list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @n: Value to be assigned to the new node.
+ *
+ * Return: Address of the new node, or NULL if it fails.
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
     dlistint_t *new_node;
