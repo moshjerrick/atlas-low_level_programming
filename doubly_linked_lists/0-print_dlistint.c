fc614a0abb7a57ed4fbba8a2afd3a83a71d1484e
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-18 14:31:17 -0500

    betty fix 0 print

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,23 @@
 #include "lists.h"
 /**
- * print_dlistint - 
- * @h: pointer to head of list
- * 
- * 
- * Return: number of nodes
+* print_dlistint - prints elements of dlistint
+* @h: pointer to head of list
+*
+*
+* Return: number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t count = 0;
+	const dlistint_t *current = h;
+	size_t count = 0;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        count++;
-    }
-    return (count);
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+
+	return (count);
 
 }
